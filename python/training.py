03b219f9c7911d8cb95e1794f8525fca29b1c419
joranhagen <hagenjoran@gmail.com>
2023-02-14 11:11:42 +0100

    done

--- python/training.py
+++ python/training.py
@@ -1,7 +1,17 @@
-def test_method():
-    # run the method to test here
-    print("testing")
+import itertools
 
 
-if __name__ == 'main':
-    test_method()
+def rgb(r, g, b):
+    def f(x): return min(255, max(x, 0))
+
+    return ("{:02X}" * 3).format(f(r), f(g), f(b))
+
+
+def get_permutations(s):
+    # return [''.join(y) for y in [x for x in set(itertools.permutations(s))]]
+    return set(''.join(x) for x in itertools.permutations(s, r=len(s)))
+
+
+if __name__ == '__main__':
+    # input function to test
+    print("Testing...")
